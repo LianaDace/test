331c9e2d882359b39ff374614fdf6895924640d4
LianaDace <105364041+LianaDace@users.noreply.github.com>
2022-07-02 22:58:16 +0300

    Update test-car.php

--- test-car.php
+++ test-car.php
@@ -9,6 +9,34 @@ class Car
         $this->fuelGauge = new FuelGauge(readline("Add max volume "));
     }
 
+    public function run()
+    {
+        while(true) {
+            echo "Choose 1 to check fuel volume " . PHP_EOL;
+            echo "Choose 2 to fill up tank " . PHP_EOL;
+            echo "Choose 3 to check odometer " . PHP_EOL;
+            echo "Choose 4 to drive " . PHP_EOL;
+            echo "Choose 0 to exit " . PHP_EOL;
+
+            $command = (int)readline();
+
+            switch ($command) {
+                case 0:
+                    echo "Bye!";
+                    die;
+                case 1:
+                    $this->getCurrentLevel();
+                    break;
+                case 2:
+                    $this->fillTank();
+                    break;
+                default:
+                    echo "Sorry, I don't understand you..";
+            }
+        }
+
+    }
+
     public function getCurrentLevel(): void
     {
         echo "Current level: " . $this->fuelGauge->getCurrentLevel() . " liter" . PHP_EOL;
@@ -18,7 +46,7 @@ class Car
     {
         $this->fuelGauge->addFuel();
     }
-    
+
 }
 
 class FuelGauge
@@ -69,11 +97,49 @@ class FuelGauge
 
 class Odometer
 {
+    private int $currentMileage;
+    private float $maxMileage;
+
+    public function __construct()
+    {
+        $this->currentMileage = 0;
+        $this->maxMileage = 999.999;
+    }
+
+    public function getCurrentMileage()
+    {
+        return $this->currentMileage;
+    }
+
+    public function addMileage()
+    {
+
+        if($this->currentMileage <= $this->maxMileage){
+            $addMileage = readline("How km would you like to drive? ");
+            for($i = 0; $i <= $addMileage; $i++){
+                return $this->currentMileage = $this->currentMileage + $addMileage;
+            }
+        }elseif($this->currentMileage > $this->maxMileage){
+            return $this->currentMileage = 0;
+        }
+        return $this->currentMileage;
+    }
 
 }
 
 $car = new Car();
+$car->run();
 $car->getCurrentLevel();
 $car->fillTank();
 $car->getCurrentLevel();
 
+
+/*
+$car2 = new Odometer();
+//$car2->addMileage();
+var_dump($car2->addMileage());
+var_dump($car2);
+
+var_dump($car2->getCurrentMileage());
+
+*/
